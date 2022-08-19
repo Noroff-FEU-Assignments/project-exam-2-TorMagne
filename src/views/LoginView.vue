de162e3bdad990fd0123524b655c1be91aa87115
Tor Magne <tormagne-brekken@outlook.com>
2022-08-19 19:00:02 +0200

    added login with auth and save localstorage

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -10,65 +10,91 @@
       justify-center
     "
   >
-    <div v-if="loading">
+    <div v-if="isLoading">
       <Loader />
     </div>
     <div v-else>
-      <label for="email" class="label"
-        ><span class="label-text-alt">Email</span></label
-      >
-      <input
-        id="email"
-        type="email"
-        placeholder="Email"
-        class="
-          input input-bordered
-          w-full
-          max-w-xs
-          border-primary
-          mb-4
-          font-raleway
-        "
-        v-model="loginData.identifier"
-      />
-      <label for="password" class="label"
-        ><span class="label-text-alt">Password</span></label
-      >
-      <input
-        id="password"
-        type="password"
-        placeholder="Password"
-        class="
-          input input-bordered
-          w-full
-          max-w-xs
-          border-primary
-          mb-4
-          font-raleway
-        "
-        v-model="loginData.password"
-      />
+      <form action="" @submit="login">
+        <label for="email" class="label"
+          ><span class="label-text-alt">Email</span></label
+        >
+        <input
+          id="email"
+          type="email"
+          placeholder="Email"
+          class="
+            input input-bordered
+            w-full
+            max-w-xs
+            border-primary
+            mb-4
+            font-raleway
+          "
+          v-model="loginData.identifier"
+        />
+        <label for="password" class="label"
+          ><span class="label-text-alt">Password</span></label
+        >
+        <input
+          id="password"
+          type="password"
+          placeholder="Password"
+          class="
+            input input-bordered
+            w-full
+            max-w-xs
+            border-primary
+            mb-4
+            font-raleway
+          "
+          v-model="loginData.password"
+        />
 
-      <button class="btn btn-primary font-sora block">Login</button>
-      <pre>{{ loginData }}</pre>
+        <button class="btn btn-primary font-sora block">Login</button>
+        <pre>{{ loginData }}</pre>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
+// components
 import Loader from "../components/Loader.vue";
+// utillity
+import axios from "axios";
 export default {
   components: {
     Loader,
   },
   data() {
     return {
-      loading: false,
+      isLoading: false,
       loginData: {
         identifier: "",
         password: "",
       },
     };
+  },
+  methods: {
+    async login(e) {
+      e.preventDefault();
+      try {
+        this.isLoading = true;
+        const response = await axios.post(
+          process.env.VUE_APP_API_URL + "auth/local",
+          this.loginData
+        );
+        const { jwt } = response.data;
+        localStorage.setItem("jwt", jwt);
+        this.$store.state.userData = this.loginData;
+        this.$store.commit("initialiseStore");
+        // console.log(this.$store.state.username);
+        this.isLoading = false;
+        this.$router.push("/guide");
+      } catch (error) {
+        this.error = error;
+      }
+    },
   },
 };
 </script>
