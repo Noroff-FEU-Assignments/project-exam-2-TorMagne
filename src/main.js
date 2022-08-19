230c28d16edc2662c0fe36d3dc4921da11e1905b
Tor Magne <tormagne-brekken@outlook.com>
2022-08-19 18:43:58 +0200

    added vuex store

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './assets/tailwind.css'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store/store';
+import './assets/tailwind.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  beforeCreate() {
+    this.$store.commit('initialiseStore');
+  },
+  render: (h) => h(App),
+}).$mount('#app');
