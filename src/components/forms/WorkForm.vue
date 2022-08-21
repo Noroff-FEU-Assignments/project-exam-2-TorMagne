2a0feee25cdfd785a140e4e18e42b315995242bb
Tor Magne <tormagne-brekken@outlook.com>
2022-08-21 09:53:53 +0200

    made work form to it's own component

---
+++ src/components/forms/WorkForm.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="container mx-auto px-4 mt-12 flex justify-center">
+    <form action="" class="w-full max-w-md">
+      <h1 class="font-sora font-bold text-3xl mb-5">Save work day</h1>
+      <!-- date -->
+      <div class="form-control w-full max-w-md">
+        <label class="label">
+          <span class="label-text font-raleway">Select work date</span>
+          {{ newWorkInfo }}
+        </label>
+        <input
+          type="date"
+          placeholder="Select work date"
+          class="
+            input input-bordered
+            border-primary
+            w-full
+            max-w-md
+            font-raleway
+            text-lg
+          "
+          v-model="newWorkInfo.data.workDate"
+        />
+      </div>
+      <!-- start time -->
+      <div class="form-control w-full max-w-md">
+        <label class="label">
+          <span class="label-text font-raleway">Write start time</span>
+        </label>
+        <input
+          type="time"
+          placeholder="Select work date"
+          class="
+            input input-bordered
+            border-primary
+            w-full
+            max-w-md
+            font-raleway
+            text-lg
+          "
+          v-model="newWorkInfo.data.startTime"
+        />
+      </div>
+      <!-- end time -->
+      <div class="form-control w-full max-w-md">
+        <label class="label">
+          <span class="label-text font-raleway">Write end time</span>
+        </label>
+        <input
+          type="time"
+          placeholder="Select work date"
+          class="
+            input input-bordered
+            border-primary
+            w-full
+            max-w-md
+            font-raleway
+            text-lg
+          "
+          v-model="newWorkInfo.data.endTime"
+        />
+      </div>
+      <div class="form-control w-full max-w-md">
+        <label class="label">
+          <span class="label-text font-raleway">Work details</span>
+        </label>
+        <textarea
+          class="textarea textarea-bordered font-raleway border-primary"
+          placeholder="Work details"
+          v-model="newWorkInfo.data.workDetails"
+        ></textarea>
+      </div>
+      <button class="btn btn-primary mt-5">Save</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newWorkInfo: {
+        data: {
+          workDate: null,
+          workStartTime: null,
+          workEndTime: null,
+          workDetails: "",
+          user: null,
+        },
+      },
+    };
+  },
+  methods: {
+    resetForm() {
+      this.newWorkInfo.data.workStartTime = "";
+      this.newWorkInfo.data.workEndTime = "";
+      this.newWorkInfo.data.workDetails = "";
+    },
+    resetSearchDate() {
+      this.searchDate = "";
+    },
+  },
+};
+</script>
--- src/views/WorkView.vue
+++ src/views/WorkView.vue
@@ -1,71 +1,12 @@
 <template>
-  <div class="container mx-auto px-4 mt-12 flex justify-center">
-    <form action="" class="w-full max-w-md">
-      <h1 class="font-sora font-bold text-3xl mb-5">Save work day</h1>
-      <!-- date -->
-      <div class="form-control w-full max-w-md">
-        <label class="label">
-          <span class="label-text font-raleway">Select work date</span>
-        </label>
-        <input
-          type="date"
-          placeholder="Select work date"
-          class="
-            input input-bordered
-            border-primary
-            w-full
-            max-w-md
-            font-raleway
-            text-lg
-          "
-        />
-      </div>
-      <!-- start time -->
-      <div class="form-control w-full max-w-md">
-        <label class="label">
-          <span class="label-text font-raleway">Write start time</span>
-        </label>
-        <input
-          type="time"
-          placeholder="Select work date"
-          class="
-            input input-bordered
-            border-primary
-            w-full
-            max-w-md
-            font-raleway
-            text-lg
-          "
-        />
-      </div>
-      <!-- end time -->
-      <div class="form-control w-full max-w-md">
-        <label class="label">
-          <span class="label-text font-raleway">Write end time</span>
-        </label>
-        <input
-          type="time"
-          placeholder="Select work date"
-          class="
-            input input-bordered
-            border-primary
-            w-full
-            max-w-md
-            font-raleway
-            text-lg
-          "
-        />
-      </div>
-      <div class="form-control w-full max-w-md">
-        <label class="label">
-          <span class="label-text font-raleway">Work details</span>
-        </label>
-        <textarea
-          class="textarea textarea-bordered font-raleway border-primary"
-          placeholder="Work details"
-        ></textarea>
-      </div>
-      <button class="btn btn-primary mt-5">Save</button>
-    </form>
-  </div>
+  <WorkForm />
 </template>
+
+<script>
+import WorkForm from "@/components/forms/WorkForm.vue";
+export default {
+  components: {
+    WorkForm,
+  },
+};
+</script>
