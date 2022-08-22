3a530d47b89c86a179dfe23f5ed70bea728b2e4d
Tor Magne <tormagne-brekken@outlook.com>
2022-08-22 11:13:36 +0200

    added success message, posting new work from form

--- src/components/forms/WorkForm.vue
+++ src/components/forms/WorkForm.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="container mx-auto px-4 mt-12 flex justify-center">
-    <form action="" class="w-full max-w-md">
+    <form action="" class="w-full max-w-md" @submit.prevent="createWorkTable">
       <Heading class="mb-5" title="Save work day" />
+      <Success v-if="success" message="You successfully saved your work" />
       <!-- date -->
       <div class="form-control w-full max-w-md">
         <label class="label">
           <span class="label-text font-raleway">Select work date</span>
-          {{ newWorkInfo }}
         </label>
         <input
           type="date"
@@ -25,7 +25,9 @@
       <!-- start time -->
       <div class="form-control w-full max-w-md">
         <label class="label">
-          <span class="label-text font-raleway">Write start time</span>
+          <span class="label-text font-raleway"
+            >Write start time {{ user.id }}</span
+          >
         </label>
         <input
           type="time"
@@ -38,7 +40,7 @@
             font-raleway
             text-lg
           "
-          v-model="newWorkInfo.data.startTime"
+          v-model="newWorkInfo.data.workStartTime"
         />
       </div>
       <!-- end time -->
@@ -57,7 +59,7 @@
             font-raleway
             text-lg
           "
-          v-model="newWorkInfo.data.endTime"
+          v-model="newWorkInfo.data.workEndTime"
         />
       </div>
       <div class="form-control w-full max-w-md">
@@ -78,9 +80,14 @@
 <script>
 // components
 import Heading from "@/components/layout/Heading.vue";
+import Success from "@/components/layout/Success.vue";
+// utility
+import axios from "axios";
+import { mapGetters } from "vuex";
 export default {
   components: {
     Heading,
+    Success,
   },
   data() {
     return {
@@ -93,9 +100,20 @@ export default {
           user: null,
         },
       },
+      success: false,
     };
   },
   methods: {
+    // show success message when form i posted
+    successFunc() {
+      this.success = true;
+      setTimeout(
+        function () {
+          this.success = false;
+        }.bind(this),
+        4000
+      );
+    },
     resetForm() {
       this.newWorkInfo.data.workStartTime = "";
       this.newWorkInfo.data.workEndTime = "";
@@ -104,6 +122,34 @@ export default {
     resetSearchDate() {
       this.searchDate = "";
     },
+    async createWorkTable() {
+      this.newWorkInfo.data.user = this.user.id;
+
+      let config = {
+        method: "post",
+        url: "work-tables",
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+          "Content-Type": "application/json",
+        },
+        data: this.newWorkInfo,
+      };
+
+      axios(config)
+        .then((response) => {
+          // this.getUserWorkTable();
+          this.resetForm();
+          this.successFunc();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  computed: {
+    ...mapGetters({
+      user: "auth/user",
+    }),
   },
 };
 </script>
---
+++ src/components/layout/Success.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="alert alert-success shadow-lg">
+    <div>
+      <span class="font-raleway">{{ message }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: String,
+  },
+};
+</script>
